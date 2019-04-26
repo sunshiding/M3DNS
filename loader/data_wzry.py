ead6d4eb6ed7c43bb2b0c8c1e8a755e86688f8de
fuzy <xjtumsu@gmail.com>
2019-04-26 17:34:53 +0800

    add data_wzry

---
+++ loader/data_wzry.py
@@ -0,0 +1,118 @@
+import os
+import pickle
+import torch
+import numpy as np
+import random
+
+def load_data(path,hp):
+    #path = '/data/yangy/data_prepare/wzry/'
+    data_num = 116
+    test_num = int(116 * 0.3)
+    semi_num = int(116 * 0.21)
+    train_num = data_num - test_num - semi_num
+    train_data = []
+    data_id = list(range(data_num))
+    train_id = random.sample(data_id,train_num,replace=False)
+    data_id = list(set(data_id)^set(train_id))
+    semi_id = random.sample(data_id,semi_num,replace=False)
+    test_id = list(set(data_id)^set(semi_id))
+
+    np.save("{}train_id.npy",train_id)
+    np.save("{}semi_id.npy",semi_id)
+    np.save("{}test_id.npy",test_id)
+
+    print("----------start reading train data----------")
+    train_data = []
+    for i in range(len(train_id)):
+        pkl_id = train_id[i]
+        filepath = path + 'wzry' + str(pkl_id) + '.pkl'
+        if os.path.exists(filepath) is True:
+            data_temp = pickle.load(open(filepath, 'rb'))
+            for key in data_temp.keys():
+                if test_is_valid(data_temp[key]):
+                    train_data.append(data_temp[key])
+    semi_data = []
+    for i in range(len(semi_id)):
+        pkl_id = semi_id[i]
+        filepath = path + 'wzry' + str(pkl_id) + '.pkl'
+        if os.path.exists(filepath) is True:
+            data_temp = pickle.load(open(filepath, 'rb'))
+            for key in data_temp.keys():
+                if test_is_valid(data_temp[key]):
+                    semi_data.append(data_temp[key])
+    test_data = []
+    for i in range(len(test_id)):
+        pkl_id = test_id[i]
+        filepath = path + 'wzry' + str(pkl_id) + '.pkl'
+        if os.path.exists(filepath) is True:
+            data_temp = pickle.load(open(filepath, 'rb'))
+            for key in data_temp.keys():
+                if test_is_valid(data_temp[key]):
+                    test_data.append(data_temp[key])
+
+    print("train data: ", len(train_data))
+    print("vali data: ", len(vali_data))
+    print("test data: ", len(test_data))
+    text_label_data, img_label_data, text_semi_data, img_semi_data = None, None, None, None
+    if ratio == 0:
+        return [train_data,text_label_data, img_label_data, semi_data,text_semi_data, img_semi_data], test_data
+    else:
+        all_label_data, text_label_data, img_label_data = get_imcomplete_data(train_data,ratio)
+        all_semi_data, text_semi_data, img_semi_data = get_imcomplete_data(semi_data,ratio)
+    return [all_label_data, text_label_data, img_label_data, all_semi_data, text_semi_data, img_semi_data], test_data
+
+def get_imcomplete_data(data,ratio):
+    ratio = ratio / 2
+    num = len(data)
+    imcomplete_num = int(num * ratio)
+    data_id = list(range(len(num)))
+    img_id = random.sample(data_id,imcomplete_num,replace=False)
+    data_id = list(set(data_id)^set(img_id))
+    text_id = random.sample(data_id,imcomplete_num,replace=False)
+    all_id = list(set(data_id)^set(text_id))
+
+    all_data = [data[i] for i in all_id]
+    img_data = [data[i] for i in img_id]
+    text_data = [data[i] for i in text_id]
+    return all_data, img_data, text_data
+
+def get_batch(data, indices):
+    view_data = [[] for i in range(view_num)]
+    bag_size = [[] for i in range(view_num)]
+    bag_label = [[] for i in range(view_num)]
+    view_num = 2
+    for i in indices:
+        for j in range(view_num):
+            instance = torch.FloatTensor(data[i][j].astype(float))
+            if j == 1:
+                instance = instance.view(-1,300)
+            else:
+                instance = instance.view(-1,3,224,224)
+            bag_size[j].append(instance.shape[0])
+            view_data[j].append(instance)
+            bag_label[j].append(torch.FloatTensor(data[i][-1].astype(float).reshape((1,-1))))
+    for i in range(view_num):
+        if len(view_data[i]) > 0:
+            view_data[i] = torch.cat(view_data[i])
+            bag_label[i] = torch.cat(bag_label[i])
+        else:
+            view_data[i] = None
+            bag_label[i] = None
+    return view_data[0],view_data[1], bag_size[0], bag_size[1], bag_label[0]
+
+def test_is_valid(data, view_num):
+    instance1_len = torch.FloatTensor(data[0].astype(float)).shape[0]
+    instance2_len = torch.FloatTensor(data[1].astype(float)).shape[0]
+    try:
+        for j in range(view_num):
+            instance = torch.FloatTensor(data[j].astype(float))
+            if j == 1:
+                instance = instance.view(-1,300)
+            else:
+                instance = instance.view(-1,3,224,224)
+            if instance1_len > 20 or instance2_len > 50:
+                return False
+        return True
+    except:
+        return False
+
--- loader/load_data.py
+++ loader/load_data.py
@@ -3,14 +3,14 @@ import os
 import pickle
 import torch
 import numpy as np
+from loader import data_wzry
+def load_data(hp):
+    datadir = '/data/yangy/data_prepare/'
+    if hp['dataname'].lower() = "wzry":
+        train_data, test_data = data_wzry.load_data(datadir,hp)
+    return train_data, test_data
 
-def load_data(data_name,ratio):
-    train_id = np.load("{}/train_id.npy".format(path))
-    semi_id = np.load("{}/semi_id.npy".format(path))
-    test_id = np.load("{}/test_id.npy".format(path))
-
-    return [all_label_data, text_label_data, img_label_data, all_semi_data, text_semi_data, img_semi_data], test_data
-
-def get_batch(data,indices):
-
+def get_batch(data,indices,hp):
+    if hp['dataname'].lower() = "wzry":
+        batch_data = data_wzry.get_batch(data,indices)
     return batch_data
